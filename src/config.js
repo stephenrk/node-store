98be1382a65a07a36ab19c3d2c5a42da40f83ffe
Stephen Knupfer <stephenrknupfer@hotmail.com>
2018-08-14 23:07:12 -0300

    updated config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ global.SALT_KEY = 'f67ef5ea-cdf1-4bff-847b-4ade9f8f3227';
 global.EMAIL_TMPL = 'Olá <strong>{0}</strong>, seja bem vindo ao Node Store!';
 
 module.exports = {
-    connectionString: 'mongodb://user:pass@ds018268.mlab.com:18268/nodestore-stephen',
+    connectionString: 'mongodb://<user>:<password>@ds018268.mlab.com:18268/nodestore-stephen',
     sendgridKey: 'TBD',
     containerConnectionString: 'TBD'
 }
