2fbcc0b1b81c6ffbe75bd0e4affcc3fe2c899a35
Stephen Knupfer <stephenknupfer@new-host.home>
2018-08-14 23:04:37 -0300

    Email sendgrid implementation, and image save

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -3,6 +3,8 @@
 // Imports
 const ValidationContract = require('../validators/fluent-validator');
 const repository = require('../repositories/product-repository');
+const azure = require('azure-storage');
+const guid = require('guid');
 
 // GET '/products'
 exports.get = async(req, res, next) => {
@@ -66,7 +68,32 @@ exports.post = async(req, res, next) => {
     }
 
     try {
-        await repository.create(req.body);
+        // Cria o Blob Service
+        const blobSvc = azure.createBlobService(config.userImagesBlobConnectionString);
+
+        let filename = guid.raw().toString() + '.jpg';
+        let rawdata = req.body.image;
+        let matches = rawdata.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+        let type = matches[1];
+        let buffer = new Buffer(matches[2], 'base64');
+
+        // Salva a imagem
+        await blobSvc.createBlockBlobFromText('product-images', filename, buffer, {
+            contentType: type
+        }, function (error, result, response) {
+            if (error) {
+                filename = 'default-product.png'
+            }
+        });
+
+        await repository.create({
+            title: req.body.title,
+            slug: req.body.slug,
+            description: req.body.description,
+            price: req.body.price,
+            tags: req.body.tags,
+            image: filename
+        });
         res.status(201).send({
             message: 'Produto cadastrado com sucesso!'
         });
